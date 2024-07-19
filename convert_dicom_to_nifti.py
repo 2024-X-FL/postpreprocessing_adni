ca3f7e20ebe65ad0bc068c0024bf9870eb7867a8
realizedr <46884833+realizedr@users.noreply.github.com>
2024-07-19 22:43:42 +0900

    Update convert_dicom_to_nifti.py

--- convert_dicom_to_nifti.py
+++ convert_dicom_to_nifti.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import math
+import nibabel as nib
 
 # 상위 디렉터리 설정
 base_dicom_folder = "/mnt/d/ADNI/extracted_files/ADNI"
@@ -19,14 +20,23 @@ else:
     processed_dirs = set()
 
 # 디렉터리 목록 가져오기
-all_dirs = [os.path.join(base_dicom_folder, d) for d in os.listdir(base_dicom_folder) if os.path.isdir(os.path.join(base_dicom_folder, d))]
+all_dirs = [os.path.join(base_dicom_folder, d) for d in os.listdir(base_dicom_folder) if
+            os.path.isdir(os.path.join(base_dicom_folder, d))]
 dirs_to_process = []
+
 
 # 변환된 파일 확인 함수
 def is_converted(dicom_dir):
-    # 변환된 NIfTI 파일이 있는지 확인 (디렉토리 이름으로 확인)
-    expected_nifti_file = os.path.join(output_folder, os.path.basename(dicom_dir) + '.nii.gz')
-    return os.path.exists(expected_nifti_file)
+    # 변환된 NIfTI 파일이 있는지 확인
+    converted_files = [f for f in os.listdir(output_folder) if f.startswith(os.path.basename(dicom_dir))]
+    for f in converted_files:
+        if f.endswith('.nii.gz'):
+            nifti_file_path = os.path.join(output_folder, f)
+            img = nib.load(nifti_file_path)
+            if len(img.shape) == 4:  # Check if the NIfTI file is 4D
+                return True
+    return False
+
 
 for dir in all_dirs:
     if dir not in processed_dirs and not is_converted(dir):
@@ -37,27 +47,48 @@ chunk_size = 60
 num_chunks = math.ceil(len(dirs_to_process) / chunk_size)
 
 for i in range(num_chunks):
-    chunk_dirs = dirs_to_process[i*chunk_size:(i+1)*chunk_size]
-    print(f"Processing chunk {i+1}/{num_chunks}: {len(chunk_dirs)} directories")
+    chunk_dirs = dirs_to_process[i * chunk_size:(i + 1) * chunk_size]
+    print(f"Processing chunk {i + 1}/{num_chunks}: {len(chunk_dirs)} directories")
 
     for dir in chunk_dirs:
         print(f"  Processing directory: {dir}")
         try:
-            result = subprocess.run(["dcm2niix", "-z", "y", "-v", "1", "-o", output_folder, dir], check=True, capture_output=True, text=True)
-            print(result.stdout)  # 변환 작업의 출력 내용을 표시
-            print(f"  Successfully processed: {dir}")
-            # 변환된 디렉터리 기록
-            with open(processed_log, 'a') as f:
-                f.write(dir + '\n')
+            result = subprocess.run(["dcm2niix", "-z", "y", "-v", "1", "-o", output_folder, dir], check=True,
+                                    capture_output=True, text=True)
+            print(result.stdout)
+
+            # 변환된 NIfTI 파일 확인
+            converted_files = [f for f in os.listdir(output_folder) if f.startswith(os.path.basename(dir))]
+            nifti_files = [f for f in converted_files if f.endswith('.nii.gz')]
+            nifti_files_4d = []
+            for nifti_file in nifti_files:
+                nifti_file_path = os.path.join(output_folder, nifti_file)
+                img = nib.load(nifti_file_path)
+                if len(img.shape) == 4:  # 4D NIfTI 파일인지 확인
+                    print(f"  Successfully processed 4D NIfTI: {nifti_file_path}")
+                    nifti_files_4d.append(nifti_file_path)
+                else:
+                    print(f"  NIfTI file is not 4D: {nifti_file_path}")
+                    os.remove(nifti_file_path)
+                    print(f"  Non-4D NIfTI file removed: {nifti_file_path}")
+
+            # 4D NIfTI 파일이 하나 이상 있는 경우에만 디렉토리를 처리된 것으로 기록
+            if nifti_files_4d:
+                with open(processed_log, 'a') as f:
+                    f.write(dir + '\n')
+            else:
+                with open(error_log, 'a') as f:
+                    f.write(f"Non-4D files in directory {dir}\n")
+
         except subprocess.CalledProcessError as e:
             print(f"  Error processing directory {dir}: {e.stderr}")
             # 오류 디렉터리 기록
             with open(error_log, 'a') as f:
-                f.write(dir + '\n')
+                f.write(f"{dir}: {e.stderr}\n")
         except Exception as e:
             print(f"  Unexpected error processing directory {dir}: {str(e)}")
             # 오류 디렉터리 기록
             with open(error_log, 'a') as f:
-                f.write(dir + '\n')
+                f.write(f"{dir}: {str(e)}\n")
 
 print("Selected directories processed.")
